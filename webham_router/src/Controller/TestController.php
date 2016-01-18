4b8d4afb423ab568e426af347fa3de9254c3f77b
Nikolay <nikolay@Nikolays-MacBook-Pro.local>
2016-01-18 16:54:42 +0100

    Reroute module add and a gitignore configuration

---
+++ webham_router/src/Controller/TestController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\webham_router\Controller\DemoController.
+ */
+
+namespace Drupal\webham_router\Controller;
+
+/**
+ * DemoController.
+ */
+class TestController {
+  /**
+   * Generates an example page.
+   */
+  public function test() {
+    return array(
+      '#markup' => t('Hello Compuccino!'),
+    );
+  }
+}
